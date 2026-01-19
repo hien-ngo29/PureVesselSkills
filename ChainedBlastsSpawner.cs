a7094316e62197b2b6a0a902eb0b77583d90f6f9
Ngo Phu Hien <ngophuhien029@gmail.com>
2026-01-19 10:03:14 +0700

    Fix blast bubbles not showing up

--- ChainedBlastsSpawner.cs
+++ ChainedBlastsSpawner.cs
@@ -2,19 +2,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using Random = UnityEngine.Random;
 using System.Collections;
+using HutongGames.PlayMaker.Actions;
 
 namespace PureVesselSkills
 {
     // Handle the blasts that spawn after you healed
     public class ChainedBlastsSpawner
     {
-        private int numberOfBlasts;
-        private Vector3 heroPos;
+        private int numberOfBlasts { get; set; }
 
         public ChainedBlastsSpawner()
         {
             numberOfBlasts = 5;
-            heroPos = HeroController.instance.gameObject.transform.position;
         }
 
         public IEnumerator SpawnChainedBlasts()
@@ -28,13 +27,21 @@ namespace PureVesselSkills
 
         private void SpawnBlastAtRandomYPlaceFromOrder(int indexOrder)
         {
+            Vector3 spawnPos = GetRandomYPositionFromOrder(indexOrder);
+
+            BlastBubble blastBubble = new BlastBubble();
+            blastBubble.SetPosition(spawnPos);
+            blastBubble.Activate();
+        }
+
+        private Vector3 GetRandomYPositionFromOrder(int indexOrder)
+        {
             Vector3 pos = new();
+            Vector3 heroPos = HeroController.instance.gameObject.transform.position;
             pos.x = heroPos.x - 20 + (8 * indexOrder) + Random.Range(-1.5f, 1.5f);
             pos.y = heroPos.y + (indexOrder % 2 != 0 ? Random.Range(11.88f, 14.08f) : Random.Range(7.88f, 10.08f)) - 7f;
 
-            BlastBubble blastBubble = new BlastBubble();
-            blastBubble.SetPosition(pos);
-            blastBubble.Activate();
+            return pos;
         }
     }
 }
